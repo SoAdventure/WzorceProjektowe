e3f361255b34ab6289e1237543c22181f304d878
SoAdventure <34894845+SoAdventure@users.noreply.github.com>
2018-03-21 23:16:58 +0100

    Singleton

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton.addValue("a");
+            Singleton.addValue("b");
+            Singleton.addValue("c");
+            Singleton.addValue("d");
+            Singleton.getList();
+            Console.WriteLine("###########");
+            Singleton.remove(2);
+            Singleton.getList();
+        }
+    }
+}
---
+++ ConsoleApp1/Singleton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonApp
+{
+    sealed class Singleton
+    {
+
+        private static readonly Singleton instance = new Singleton();
+        static List<string> _list;
+        private Singleton()
+        {
+            _list = new List<string>();
+        }
+        static Singleton()
+        {
+            
+        }
+        public static Singleton Instance
+        {
+            get {
+                return instance;
+            }
+        }
+
+        public static void addValue ( String value )
+        {
+            _list.Add(value);
+        }
+        
+        public static void remove( int index)
+        {
+            _list.RemoveAt( index );
+        }
+
+        public static void getList()
+        {
+            foreach (var value in _list)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+
+    }
+}
