02087e03b86dde89d4074ff93c86ee1f3d2ed393
SoAdventure <34894845+SoAdventure@users.noreply.github.com>
2018-03-21 23:42:23 +0100

    Początek Factory

--- Program.cs
+++ Program.cs
@@ -6,6 +6,33 @@ using System.Threading.Tasks;
 
 namespace factory
 {
+
+    interface IDoTask
+    {
+        void DoTaskOne();
+    }
+    
+    public class AddItems : IDoTask
+    {
+        
+        public void DoTaskOne()
+        {
+            Console.WriteLine("Dodaj");
+        }
+    }
+    public class ListItems
+    {
+        private IDoTask task;
+        public ListItems()
+        {
+            
+        }
+        
+        public void EndTheIssue()
+        {
+            task.DoTaskOne();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
