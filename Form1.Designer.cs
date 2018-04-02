f176934b13d8c5e904cad8c42892d474c247ae33
SoAdventure <roksaon@gmail.com>
2018-04-02 20:50:28 +0200

    Okienko

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dodaj = new System.Windows.Forms.Button();
+            this.usuń = new System.Windows.Forms.Button();
+            this.sortuj = new System.Windows.Forms.Button();
+            this.tabela = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tabela)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dodaj
+            // 
+            this.dodaj.Location = new System.Drawing.Point(268, 9);
+            this.dodaj.Name = "dodaj";
+            this.dodaj.Size = new System.Drawing.Size(75, 23);
+            this.dodaj.TabIndex = 0;
+            this.dodaj.Text = "dodaj";
+            this.dodaj.UseVisualStyleBackColor = true;
+            this.dodaj.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // usuń
+            // 
+            this.usuń.Location = new System.Drawing.Point(268, 38);
+            this.usuń.Name = "usuń";
+            this.usuń.Size = new System.Drawing.Size(75, 23);
+            this.usuń.TabIndex = 1;
+            this.usuń.Text = "usuń";
+            this.usuń.UseVisualStyleBackColor = true;
+            // 
+            // sortuj
+            // 
+            this.sortuj.Location = new System.Drawing.Point(39, 87);
+            this.sortuj.Name = "sortuj";
+            this.sortuj.Size = new System.Drawing.Size(304, 23);
+            this.sortuj.TabIndex = 2;
+            this.sortuj.Text = "sortuj";
+            this.sortuj.UseVisualStyleBackColor = true;
+            // 
+            // tabela
+            // 
+            this.tabela.AllowUserToAddRows = false;
+            this.tabela.AllowUserToDeleteRows = false;
+            this.tabela.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabela.Location = new System.Drawing.Point(12, 128);
+            this.tabela.MultiSelect = false;
+            this.tabela.Name = "tabela";
+            this.tabela.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tabela.Size = new System.Drawing.Size(331, 350);
+            this.tabela.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 12);
+            this.textBox1.MaxLength = 45;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 38);
+            this.textBox2.MaxLength = 45;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "imie";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "nazwisko ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(360, 507);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tabela);
+            this.Controls.Add(this.sortuj);
+            this.Controls.Add(this.usuń);
+            this.Controls.Add(this.dodaj);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tabela)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button dodaj;
+        private System.Windows.Forms.Button usuń;
+        private System.Windows.Forms.Button sortuj;
+        private System.Windows.Forms.DataGridView tabela;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
