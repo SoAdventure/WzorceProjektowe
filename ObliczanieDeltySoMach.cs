30bb910ee5fa7ae6bdb1ebfbbff34894171bd195
SoAdventure <roksaon@gmail.com>
2018-05-08 12:05:03 +0200

    Obliczenia do delty <3

--- ObliczanieDeltySoMach.cs
+++ ObliczanieDeltySoMach.cs
@@ -8,5 +8,21 @@ namespace facade
 {
     class ObliczanieDeltySoMach
     {
+        public void DeltaDod(double a, double b, double c)
+        {
+            double delta = b * b - 4.0 * a * c;
+            Console.WriteLine("Pierwszy pierwiastek: {0}", (-b - delta) / (2 * a));
+            Console.WriteLine("Drugi pierwiastek: {0}", (-b + delta) / (2 * a));
+        }
+
+        public void DeltaZerowa(double a, double b, double c)
+        {
+            Console.WriteLine("Pierwiastek: {0}", (-b) / (2 * a));
+        }
+
+        public void DeltaUj(double a, double b, double c)
+        {
+            Console.WriteLine("Brak pierwiastków wielomianu kwadratowego");
+        }
     }
 }
