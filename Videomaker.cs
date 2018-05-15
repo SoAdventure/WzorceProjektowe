b44f4852c6c59bf2a9dc16836c36ad241c3cebf6
SoAdventure <roksaon@gmail.com>
2018-05-15 22:25:44 +0200

    videomaker

---
+++ Videomaker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Videomaker : Employee
+    {
+        public Videomaker(string surname)
+        {
+            this.Surname = surname;
+        }
+
+        public override void WykonajZadanie(Tasks task)
+        {
+           
+            if (task == Tasks.Video)
+            {
+                Console.WriteLine("{0} o imieniu {1} obsługuje zadanie: {2}",
+                    this.GetType().Name, Surname, task);
+            }
+            else if (next != null)
+            {
+                next.WykonajZadanie(task);
+            }
+        }
+    }
+}
