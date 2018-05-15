34abf3bf430a71d80c0a3c997cb6b97634e1103e
SoAdventure <roksaon@gmail.com>
2018-05-15 21:53:18 +0200

    Employee

---
+++ Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    abstract class Employee
+    {
+         
+        
+            public string Name { get; set; }
+
+            
+            protected Employee next;
+
+            
+            
+            public void UstawKolejnego(Employee employee)
+            {
+                next = employee;
+            }
+
+          
+            public abstract void WykonajZadanie(Tasks task);
+        
+    }
+}
