077f7a9d8b1a4f6aa5f3e1cb771aebdb0309d254
SoAdventure <roksaon@gmail.com>
2018-05-15 22:37:05 +0200

    ProgramMain

--- Program.cs
+++ Program.cs
@@ -10,6 +10,30 @@ namespace ChainOfResponsibility
     {
         static void Main(string[] args)
         {
+
+
+            Employee menagerBruceLee = new Manager("Bruce Lee");
+            Employee photographer = new Photographer("Chuck Norris");
+            Employee videomaker = new Videomaker("Maciek z Klanu");
+
+
+            menagerBruceLee.UstawKolejnego(photographer);
+            photographer.UstawKolejnego(videomaker);
+
+
+            List<Tasks> zadaniaNaDzis = new List<Tasks> {
+                Tasks.Video,
+                Tasks.Print,
+                Tasks.Photo,
+                Tasks.Other
+            };
+
+
+            foreach (var task in zadaniaNaDzis)
+            {
+                menagerBruceLee.WykonajZadanie(task);
+            }
+
         }
     }
 }
