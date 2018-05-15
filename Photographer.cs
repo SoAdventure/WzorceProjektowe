9152649d9f7903dc1dbe1809d6f247ff795d2f4f
SoAdventure <roksaon@gmail.com>
2018-05-15 22:22:25 +0200

    photographer

---
+++ Photographer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Photographer : Employee
+    {
+        public Photographer (string surname)
+        {
+            this.Surname = surname;
+        }
+
+        public override void WykonajZadanie(Tasks task)
+        {
+            
+            if (task == Tasks.Photo)
+            {
+                Console.WriteLine("{0} o imieniu {1} obsługuje zadanie: {2}",
+                    this.GetType().Name, Surname, task);
+            }
+            else if (next != null)
+            {
+                next.WykonajZadanie(task);
+            }
+        }
+    }
+}
