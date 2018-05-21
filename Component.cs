4f9a4bdf9f2f382cabd3da2f4ce9e238e43e84e8
SoAdventure <roksaon@gmail.com>
2018-05-21 21:34:03 +0200

    Komponent

---
+++ Component.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    interface Component
+    {
+        int Ocena { get; set; }
+        string NazwaPrzedmiotu { get; set; }
+
+        void Dodaj(Component ob);
+        void Usun(Component ob);
+        void Wyswietl(int nextOp);
+    }
+}
